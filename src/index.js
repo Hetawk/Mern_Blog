10cfef2a99d8540e5b67049487d8cce445023060
ENOCH KWATEH DONGBO <enoch.dongbo@gmail.com>
2022-07-19 18:07:44 +0800

    config file

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ContextProvider>
-    <App />
+      <App />
     </ContextProvider>
   </React.StrictMode>
-  // , document.getElementById("root")
-); 
+  // , document.getElementById("root") 
+
+);
 
 
