f09d41602940c09cada5643802b4c5bfb83c4526
ENOCH KWATEH DONGBO <enoch.dongbo@gmail.com>
2022-07-26 17:17:41 +0800

    proxy commit 3

--- api/setupProxy.js
+++ api/setupProxy.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-//initializing proxy
+///initializing proxy
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 //proxy
